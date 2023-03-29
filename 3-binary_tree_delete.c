6cbd0a131c802545427f4521c3932b38a92312f5
Frankenstein9585 <donny9585@gmail.com>
2023-03-29 16:33:24 +0100

    Done task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,14 @@
+/**
+ * binary_tree_delete - deletes an entire binary tree
+ *
+ * @tree: pointer to the root node of the tree to be deleted
+ *
+ */
+#include "binary_trees.h"
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+
+	free(tree);
+}
