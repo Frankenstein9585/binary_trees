60b2e567365f46362785ecb23aef4e2b3d0b02ce
Frankenstein9585 <donny9585@gmail.com>
2023-03-30 17:27:58 +0100

    Modified again

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,6 +6,8 @@
  * Return: return the height of the tree otherwise NULL
  */
 #include "binary_trees.h"
+size_t binary_tree_height2(const binary_tree_t *tree);
+
 size_t binary_tree_height2(const binary_tree_t *tree)
 {
 	if (tree == NULL)
