3be244636f174cc18447d4d493c2536085678f65
Frankenstein9585 <donny9585@gmail.com>
2023-03-30 12:39:48 +0100

    Done task 7

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,17 @@
+/**
+ * binary_tree_inorder - goes through a binary tree using inorder traversal
+ *
+ * @tree: pointer to the root node of the tree
+ * @func: pointer to a function to call for each node
+ *
+ */
+#include "binary_trees.h"
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
+}
