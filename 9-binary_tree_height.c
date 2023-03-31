d1f916697bba76a33135390bb26775b7626260d0
Frankenstein9585 <donny9585@gmail.com>
2023-03-31 14:43:06 +0100

    Task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -20,18 +20,3 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	return (right_height + 1);
 }
-
-/**
- * binary_tree_heightx - measures the height of a binary tree
- *
- * @tree: pointer to the root node of the tree
- *
- * Return: the height of the tree otherwise NULL
- */
-size_t binary_tree_heightx(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-
-	return (binary_tree_height2(tree) - 1);
-}
